b12c5903e694aaf0d0d723b17a0dbe707cbe5e84
Matthew Lehr <matthewslehr@gmail.com>
2024-03-13 20:35:28 +0000

    added index.html to s3 bucket as a part of iac

--- iac/terraform/s3.tf
+++ iac/terraform/s3.tf
@@ -1,5 +1,16 @@
-resource "aws_s3_bucket" "my-s3-bucket" {
+resource "aws_s3_bucket" "book-swap-bucket" {
   tags = {
     project = var.project
   }
+}
+
+resource "aws_s3_object" "object" {
+  bucket = aws_s3_bucket.book-swap-bucket.id
+  key    = "index.html"
+  source = "../../frontend/index.html"
+
+  # terraform cannot tell when object has changed.. unless you use the etag
+  # etag = filemd5("path/to/file")
+  etag = filemd5("../../frontend/index.html")
+  
 }
