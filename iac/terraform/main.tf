42ce1fb3c4a9f8d368569e67a2339a32f301a0f2
Matthew Lehr <matthewslehr@gmail.com>
2024-03-14 14:11:11 +0000

    Got website working

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable project {
+variable proj {
     type = string
     default = "book-swap"
 }
--- iac/terraform/s3.tf
+++ iac/terraform/s3.tf
@@ -1,16 +1,62 @@
-resource "aws_s3_bucket" "book-swap-bucket" {
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "book-swap-frontend-bucket"
+
   tags = {
-    project = var.project
+    project = var.proj
   }
 }
 
-resource "aws_s3_object" "object" {
-  bucket = aws_s3_bucket.book-swap-bucket.id
-  key    = "index.html"
-  source = "../../frontend/index.html"
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "PublicReadGetObject",
+          "Effect" : "Allow",
+          "Principal" : "*",
+          "Action" : "s3:GetObject",
+          "Resource" : "arn:aws:s3:::${aws_s3_bucket.frontend_bucket.id}/*"
+        }
+      ]
+    }
+  )
+}
 
-  # terraform cannot tell when object has changed.. unless you use the etag
-  # etag = filemd5("path/to/file")
-  etag = filemd5("../../frontend/index.html")
+#resource "aws_s3_bucket_public_access_block" "frontend_bucket_public_access" {
+#  bucket = aws_s3_bucket.frontend_bucket.id
+#  block_public_acls       = false
+#  block_public_policy     = false
+#  ignore_public_acls      = false
+#  restrict_public_buckets = false
+#}
+
+
+resource "aws_s3_bucket_website_configuration" "hosting" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  #error_document {
+  #  key = "error.html"
+  #}
+
+}
+
+resource "aws_s3_object" "object" {
+  for_each     = fileset(path.module, "frontend/**/*.{html,css,js}")
+  bucket       = aws_s3_bucket.frontend_bucket.id
+  key          = replace(each.value, "/^frontend//", "")
+  source       = each.value
+  content_type = lookup(local.content_types, regex("\\.[^.]+$", each.value), null)
+  etag         = filemd5(each.value)
+
+  #bucket = aws_s3_bucket.frontend_bucket.id
+  #key    = "index.html"
+  #source = "../../frontend/index.html"
+  #etag = filemd5("../../frontend/index.html")
   
 }
